1dedb10a6c9d083771cbbf215b7475792c5169a7
bwire40 <emmanuelbwire80@gmail.com>
2023-11-20 10:09:27 +0300

    Edited the terms page

--- resources/views/partials/left_navbar.blade.php
+++ resources/views/partials/left_navbar.blade.php
@@ -1,27 +1,33 @@
 <div class="w-full border-2 border-slate-300 flex flex-col justify-center items-center">
-    <div class="pt-3">
-        <ul class="flex-col gap-2">
-            <li class="py-2 font-bold transition-all duration-300 hover:text-slate-600">
-                <a class=" text-black" href="{{ route('home') }}">
+    <div class="pt-3 w-full">
+        <ul class="flex-col items-center justify-center gap-2 w-full">
+            <li
+                class="flex flex-col items-center justify-center py-2 font-bold transition-all duration-300 hover:text-slate-300 hover:bg-slate-800">
+                <a class=" text-blue-600" href="{{ route('home') }}">
                     <span>Home</span></a>
             </li>
-            <li class="py-2 font-bold transition-all duration-300 hover:text-slate-600">
+            <li
+                class="flex flex-col items-center justify-center py-2 font-bold transition-all duration-300 hover:text-slate-300 hover:bg-slate-800">
                 <a class="" href="#">
                     <span>Explore</span></a>
             </li>
-            <li class="py-2 font-bold transition-all duration-300 hover:text-slate-600">
+            <li
+                class="flex flex-col items-center justify-center py-2 font-bold transition-all duration-300 hover:text-slate-300 hover:bg-slate-800">
                 <a class="" href="#">
                     <span>Feed</span></a>
             </li>
-            <li class="py-2 font-bold transition-all duration-300 hover:text-slate-600">
+            <li
+                class="flex flex-col items-center justify-center py-2 font-bold transition-all duration-300 hover:text-slate-300 hover:bg-slate-800">
                 <a class="" href="{{ route('terms') }}">
                     <span>Terms</span></a>
             </li>
-            <li class="py-2 font-bold transition-all duration-300 hover:text-slate-600">
+            <li
+                class="flex flex-col items-center justify-center py-2 font-bold transition-all duration-300 hover:text-slate-300 hover:bg-slate-800">
                 <a class="" href="#">
                     <span>Support</span></a>
             </li>
-            <li class="py-2 font-bold transition-all duration-300 hover:text-slate-600">
+            <li
+                class="flex flex-col items-center justify-center py-2 font-bold transition-all duration-300 hover:text-slate-300 hover:bg-slate-800">
                 <a class="" href="#">
                     <span>Settings</span></a>
             </li>
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,53 +1,68 @@
 @extends('layout.layout')
 @section('content')
-    <div>
-        <h1 class="text-3xl font-bold underline text-slate-600">Terms and conditions</h1>
-        <p class="text-slate-500 text-justify py-4">Sint elit qui laborum eiusmod ipsum ea sit do commodo Lorem deserunt
-            velit.
-            Nostrud
-            officia nostrud consequat
-            esse duis excepteur cillum culpa fugiat officia fugiat laboris nostrud. Officia ea ipsum incididunt esse do
-            cupidatat. Deserunt duis voluptate veniam mollit Lorem aute reprehenderit in nostrud proident exercitation
-            reprehenderit ullamco dolor. Nisi quis reprehenderit Lorem anim cillum incididunt.
-        </p>
-        <p>
-            Aliqua dolore in labore ut. Nulla enim commodo officia officia sit aliquip. Ea ex laborum proident ex mollit
-            labore duis amet id excepteur. Duis veniam ad esse aliquip sunt anim dolor Lorem et sit est. Velit eiusmod
-            voluptate aliquip dolore excepteur ullamco voluptate sit cillum anim culpa.
+    <div class="px-4 lg:px-0 flex flex-col lg:flex-row justify-center items-center lg:items-start w-full py-4">
 
-            Lorem eiusmod culpa duis dolor eu dolore exercitation irure. Ut et laborum dolore aute velit ad cillum sunt
-            voluptate. Culpa fugiat cupidatat elit aliqua quis elit in. Reprehenderit minim nostrud nulla consectetur nulla
-            laborum mollit consectetur ut non adipisicing veniam aliqua.
-        </p>
-        Sit officia cillum exercitation cupidatat duis tempor pariatur laborum velit commodo reprehenderit incididunt
-        in. Labore laborum labore nostrud anim adipisicing. Nostrud non non id nulla incididunt nisi exercitation
-        consectetur. Nisi veniam ullamco non excepteur non in quis anim tempor quis cillum eiusmod mollit veniam.
-        Eiusmod quis sit quis excepteur consequat deserunt magna enim voluptate aute. Dolore ipsum Lorem incididunt
-        mollit mollit anim proident excepteur occaecat. Quis non voluptate duis esse ad adipisicing cillum dolor commodo
-        dolore non.
+        {{-- left navbar --}}
+        <div class="hidden lg:block lg:w-3/12">
+            {{-- left navbar --}}
+            @include('partials.left_navbar')
+        </div>
 
-        Adipisicing ex enim dolor ex enim. Pariatur tempor irure ullamco voluptate velit id et nisi nostrud ex cillum.
-        Non do anim sint anim consectetur esse nisi aliqua.
-        <p>
-            Nostrud anim esse laboris consequat aute laboris quis qui officia. Lorem aliquip sint exercitation ex labore
-            proident esse. Minim sunt anim qui Lorem aliquip culpa esse ipsum qui sit proident id reprehenderit. Id elit
-            consequat voluptate cillum culpa. Occaecat id esse laboris est id quis velit duis incididunt cupidatat qui qui.
+        <div class="w-full px-4 lg:w-6/12 mx-4">
+            <h1 class="text-3xl font-bold underline text-slate-600">Terms and conditions</h1>
+            <p class="text-slate-500 text-justify py-4">Sint elit qui laborum eiusmod ipsum ea sit do commodo Lorem deserunt
+                velit.
+                Nostrud
+                officia nostrud consequat
+                esse duis excepteur cillum culpa fugiat officia fugiat laboris nostrud. Officia ea ipsum incididunt esse do
+                cupidatat. Deserunt duis voluptate veniam mollit Lorem aute reprehenderit in nostrud proident exercitation
+                reprehenderit ullamco dolor. Nisi quis reprehenderit Lorem anim cillum incididunt.
+            </p>
+            <p>
+                Aliqua dolore in labore ut. Nulla enim commodo officia officia sit aliquip. Ea ex laborum proident ex mollit
+                labore duis amet id excepteur. Duis veniam ad esse aliquip sunt anim dolor Lorem et sit est. Velit eiusmod
+                voluptate aliquip dolore excepteur ullamco voluptate sit cillum anim culpa.
 
-            Ad enim mollit do mollit. Amet aute anim irure laborum nisi labore ad. Eu quis Lorem amet mollit ut laborum
-            cupidatat tempor laboris amet in. Reprehenderit proident excepteur sunt reprehenderit et aliqua eu officia. Amet
-            culpa magna dolor id aute nulla laboris do aliqua quis laborum nisi et.
+                Lorem eiusmod culpa duis dolor eu dolore exercitation irure. Ut et laborum dolore aute velit ad cillum sunt
+                voluptate. Culpa fugiat cupidatat elit aliqua quis elit in. Reprehenderit minim nostrud nulla consectetur
+                nulla
+                laborum mollit consectetur ut non adipisicing veniam aliqua.
+            </p>
+            Sit officia cillum exercitation cupidatat duis tempor pariatur laborum velit commodo reprehenderit incididunt
+            in. Labore laborum labore nostrud anim adipisicing. Nostrud non non id nulla incididunt nisi exercitation
+            consectetur. Nisi veniam ullamco non excepteur non in quis anim tempor quis cillum eiusmod mollit veniam.
+            Eiusmod quis sit quis excepteur consequat deserunt magna enim voluptate aute. Dolore ipsum Lorem incididunt
+            mollit mollit anim proident excepteur occaecat. Quis non voluptate duis esse ad adipisicing cillum dolor commodo
+            dolore non.
 
-            Incididunt sunt consectetur cupidatat Lorem veniam. Sint quis veniam ipsum id. Eu qui voluptate sit proident
-            duis sint.
-        </p>
-        <p>
-            Ipsum occaecat anim esse consequat enim do elit non labore in. Fugiat aliquip sint tempor et reprehenderit
-            labore mollit ex. Anim do commodo consequat dolore. Nostrud ipsum sunt nisi enim eu do non irure magna non dolor
-            reprehenderit in.
+            Adipisicing ex enim dolor ex enim. Pariatur tempor irure ullamco voluptate velit id et nisi nostrud ex cillum.
+            Non do anim sint anim consectetur esse nisi aliqua.
+            <p>
+                Nostrud anim esse laboris consequat aute laboris quis qui officia. Lorem aliquip sint exercitation ex labore
+                proident esse. Minim sunt anim qui Lorem aliquip culpa esse ipsum qui sit proident id reprehenderit. Id elit
+                consequat voluptate cillum culpa. Occaecat id esse laboris est id quis velit duis incididunt cupidatat qui
+                qui.
 
-            Eu amet ullamco reprehenderit aute fugiat ex. Non dolore mollit commodo est excepteur est aute ut proident
-            pariatur cillum ipsum quis. Eu minim excepteur anim fugiat. Excepteur culpa duis officia et sunt id quis veniam
-            occaecat veniam pariatur tempor magna consequat. Ex amet in laborum adipisicing in officia consequat minim. Amet
-            velit ullamco irure duis elit adipisicing veniam nostrud tempor in.</p>
+                Ad enim mollit do mollit. Amet aute anim irure laborum nisi labore ad. Eu quis Lorem amet mollit ut laborum
+                cupidatat tempor laboris amet in. Reprehenderit proident excepteur sunt reprehenderit et aliqua eu officia.
+                Amet
+                culpa magna dolor id aute nulla laboris do aliqua quis laborum nisi et.
+
+                Incididunt sunt consectetur cupidatat Lorem veniam. Sint quis veniam ipsum id. Eu qui voluptate sit proident
+                duis sint.
+            </p>
+            <p>
+                Ipsum occaecat anim esse consequat enim do elit non labore in. Fugiat aliquip sint tempor et reprehenderit
+                labore mollit ex. Anim do commodo consequat dolore. Nostrud ipsum sunt nisi enim eu do non irure magna non
+                dolor
+                reprehenderit in.
+
+                Eu amet ullamco reprehenderit aute fugiat ex. Non dolore mollit commodo est excepteur est aute ut proident
+                pariatur cillum ipsum quis. Eu minim excepteur anim fugiat. Excepteur culpa duis officia et sunt id quis
+                veniam
+                occaecat veniam pariatur tempor magna consequat. Ex amet in laborum adipisicing in officia consequat minim.
+                Amet
+                velit ullamco irure duis elit adipisicing veniam nostrud tempor in.</p>
+        </div>
     </div>
 @endsection
